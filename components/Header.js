29c54ad9b20608a0509121c86914e60687842dcb
afifudinmtop <afifudinmaarif@gmail.com>
2022-01-20 08:36:18 +0700

    v1

---
+++ components/Header.js
@@ -0,0 +1,44 @@
+function Header() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light shadow">
+        <div className="container-fluid">
+          <a class="navbar-brand d-flex flex-row" href="#">
+            <img src="./pic/1.png" width="30" />
+            <div className="fw-bold ms-2">React</div>
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Pricing
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
